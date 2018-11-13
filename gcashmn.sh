4a7d16c619561a0889f8ed5a1cccc64943d0d228
patrykcc <42496973+patrykcc@users.noreply.github.com>
2018-11-13 22:38:49 +0100

    Update gcashmn.sh

--- gcashmn.sh
+++ gcashmn.sh
@@ -4,7 +4,7 @@ CONFIG_FILE='gigacash.conf'
 CONFIGFOLDER='/root/.gigacash'
 COIN_DAEMON='/usr/local/bin/gigacashd'
 COIN_CLI='/usr/local/bin/gigacash-cli'
-COIN_REPO='https://github.com/GigaCash/GigaCash-Masternode-Setup/blob/master/gigacash.tar.gz'
+COIN_REPO='https://raw.githubusercontent.com/GigaCash/GigaCash-Masternode-Setup/master/gigacash.tar.gz'
 COIN_NAME='Gigacash'
 COIN_PORT=5078
 
