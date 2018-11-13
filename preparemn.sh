8cded22b06655d1fdcefae4dca2ecb14b099f069
patrykcc <42496973+patrykcc@users.noreply.github.com>
2018-11-13 22:55:41 +0100

    Update preparemn.sh

--- preparemn.sh
+++ preparemn.sh
@@ -1,6 +1,3 @@
 apt-get update
-apt-get install wget -y
-apt-get install net-tools
-apt-get install lsb-release -y
-apt-get install curl -y
 apt-get upgrade -y
+apt-get -y install vim unzip nano wget net-tools lsb-release curl
