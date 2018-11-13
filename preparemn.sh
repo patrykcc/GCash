9b2d4a34b9fa2211506df323ce4f8b617a368776
patrykcc <42496973+patrykcc@users.noreply.github.com>
2018-11-13 22:52:56 +0100

    Update preparemn.sh

--- preparemn.sh
+++ preparemn.sh
@@ -1,10 +1,6 @@
 apt-get update
 apt-get install wget -y
-apt-get install git -y
+apt-get install net-tools
 apt-get install lsb-release -y
 apt-get install curl -y
 apt-get upgrade -y
-
-git clone https://github.com/patrykcc/GCash
-cd GCash
-bash gcashmn.sh
